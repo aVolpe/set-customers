e93fc287845cd355be3fc014b2baf098ecabcb49
Arturo Volpe <arturovolpe@gmail.com>
2016-08-16 16:30:56 -0400

    Change downloader to act as a module

--- downloader.rb
+++ downloader.rb
@@ -6,37 +6,47 @@ require 'uri'
 require 'net/http'
 require 'fileutils'
 
-TEMP_FOLDER = '.tmp'
+module Downloader
 
-def create_temp_folder()
-  FileUtils.mkdir_p TEMP_FOLDER
-end
+  TEMP_FOLDER = '.tmp'
 
-def get_file_path(name)
-  return "#{TEMP_FOLDER}/#{name}"
-end
-
-def save_to_tempfile(url, name)
-  uri = URI.parse(url)
-  Net::HTTP.start(uri.host, uri.port) do |http|
-    resp = http.get(uri.path)
-    file = File.new(get_file_path(name), 'wb+')
-    file.binmode
-    file.write(resp.body)
-    file.flush
-    file
+  def self.create_temp_folder()
+    FileUtils.mkdir_p TEMP_FOLDER
   end
-end
 
-
-create_temp_folder
-SETUrls::DATA.each_with_index do |url, index| 
-  name = "ruc#{index}.zip";
-  if not File.exist?(get_file_path(name)) 
-    puts "Downloading index #{index} (#{url})"
-    save_to_tempfile(url, name)
-  else 
-    puts "Skiping index #{index}, file already exists"
+  def self.get_file_path(name)
+    return "#{TEMP_FOLDER}/#{name}"
   end
-end
 
+  def self.save_to_tempfile(url, name)
+    uri = URI.parse(url)
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      resp = http.get(uri.path)
+      file = File.new(get_file_path(name), 'wb+')
+      file.binmode
+      file.write(resp.body)
+      file.flush
+      file
+    end
+  end
+
+  def self.download()
+
+    self.create_temp_folder
+    SETUrls::DATA.each_with_index do |url, index| 
+      name = "ruc#{index}.zip";
+      if not File.exist?(self.get_file_path(name)) 
+        puts "Downloading index #{index} (#{url})"
+        self.save_to_tempfile(url, name)
+      else 
+        puts "Skiping index #{index}, file already exists"
+      end
+    end
+
+  end
+
+  if __FILE__ == $0
+    self.download
+  end
+
+end
