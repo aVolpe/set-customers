ec7000d49eeea34dda6125accdce344c408406cc
Arturo Volpe <arturovolpe@gmail.com>
2016-08-16 16:22:37 -0400

    Fix download urls

--- rucs.rb
+++ rucs.rb
@@ -1,8 +1,15 @@
 #!/usr/bin/ruby
 
-# The URL's to download the customer data.
-DATA = [];
+module SETUrls
 
-(0..9).each do |termination|
-  DATA.push("http://www.set.gov.py/portal/PARAGUAY-SET/detail?folder-id=repository:collaboration:/sites/PARAGUAY-SET/categories/SET/Informes%20Periodicos/listado-de-ruc-con-sus-equivalencias&content-id=/repository/collaboration/sites/PARAGUAY-SET/documents/informes-periodicos/ruc/ruc#{termination}.zip")
+  # The URL's to download the customer data.
+  DATA = [];
+  # this is the current version
+  VERSION = 9;
+
+
+  (0..9).each do |termination|
+    DATA.push("http://www.set.gov.py/rest/contents/download/collaboration/sites/PARAGUAY-SET/documents/informes-periodicos/ruc/ruc#{termination}.zip?version=#{VERSION}")
+  end
+
 end
